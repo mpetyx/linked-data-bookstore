0dd2ae253a23084e196bd6483f6059974a895c06
Gkastrin <george.kastrinakis91@gmail.com>
2014-04-26 21:39:29 +0300

    Create AdvancedSearchPage.js

---
+++ AdvancedSearchPage.js
@@ -0,0 +1,63 @@
+
+$("#nap1").click(function(event){
+	
+	event.stopPropagation();
+	event.preventDefault();
+
+});
+	
+
+$("#nap2").click(function(event){
+});
+
+	
+$("#nap3").click(function(event){
+	event.stopPropagation();
+	event.preventDefault();
+});
+	
+$("#nap4").click(function(event){
+	
+	event.stopPropagation();
+	event.preventDefault();
+
+});
+	
+
+$("#nap5").click(function(event){
+});
+
+	
+$("#nap6").click(function(event){
+	
+	event.stopPropagation();
+	event.preventDefault();
+
+});
+	
+
+$("#nap7").click(function(event){
+});
+
+	
+$("#nap8").click(function(event){
+	
+	event.stopPropagation();
+	event.preventDefault();
+
+});
+	
+
+$("#nap9").click(function(event){
+});
+
+	
+$("#nap10").click(function(event){
+	
+	event.stopPropagation();
+	event.preventDefault();
+
+});
+	
+$("#nap12").draggable();
+	
